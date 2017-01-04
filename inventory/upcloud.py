ec70baf69f642e87a7572179c73377daa9aebe61
Ville Savolainen <ville.savolainen@dovecot.fi>
2017-01-04 11:16:38 +0200

    changed get_IPs() to get_ips() and server_uuid to server because of earlier refactoring in upcloud-api python module

--- inventory/upcloud.py
+++ inventory/upcloud.py
@@ -113,9 +113,9 @@ def assign_ips_to_servers(servers):
         object.__setattr__(server, 'ip_addresses', [])
 
     # assign IPs to their corresponding server
-    ips = manager.get_IPs()
+    ips = manager.get_ips()
     for ip in ips:
-        servermap[ip.server_uuid].ip_addresses.append(ip)
+        servermap[ip.server].ip_addresses.append(ip)
 
 
 def list_servers(manager, get_ip_address=False):
